b5b0da9797a3e7f26bd46e323518b3a57a1bd7b9
Kerawos <Maresowa@gmail.com>
2017-11-07 20:21:48 +0100

    @ update .html Form

--- src/main/java/pl/akademiakodu/MethodsExerciseSpring/controllers/MainController.java
+++ src/main/java/pl/akademiakodu/MethodsExerciseSpring/controllers/MainController.java
@@ -1,20 +1,34 @@
 package pl.akademiakodu.MethodsExerciseSpring.controllers;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pl.akademiakodu.MethodsExerciseSpring.models.forms.ExerciseScreen;
+import pl.akademiakodu.MethodsExerciseSpring.models.forms.FileToString;
 
 import java.io.IOException;
 
 @Controller
 public class MainController {
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String indexGet(Model model) {
-        model.addAttribute("displayExercise", new ExerciseScreen());
+    @GetMapping("/")
+    public String indexGet(Model model) throws IOException {
+        model.addAttribute("message", FileToString.readFile("description.txt"));
         return "displayExercise";
     }
 
+//    @PostMapping("/")
+//    public String index(@ModelAttribute("displayExercise") ExerciseScreen form, Model model) {
+//        model.addAttribute("message", "asdfasdfasdfsaf");
+//        return "displayExercise";
+//    }
+
+
+
 }
+
+
