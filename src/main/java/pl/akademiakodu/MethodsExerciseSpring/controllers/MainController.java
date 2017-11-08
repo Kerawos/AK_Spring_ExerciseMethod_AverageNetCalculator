f23f75a9582b2f28d4bdff59f68eabcba857be9b
Kerawos <Maresowa@gmail.com>
2017-11-08 09:50:50 +0100

    @ update navigation

--- src/main/java/pl/akademiakodu/MethodsExerciseSpring/controllers/MainController.java
+++ src/main/java/pl/akademiakodu/MethodsExerciseSpring/controllers/MainController.java
@@ -5,17 +5,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.akademiakodu.MethodsExerciseSpring.models.services.ReadFileToString;
 
+
 import java.io.IOException;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String indexGet(Model model) throws IOException {
+    public String indexWelcome(Model model) throws IOException {
+        return "welcomeScreen";
+    }
+
+    @GetMapping("/desc")
+    public String indexDesc(Model model) throws IOException {
         //display to user *dexcription
         model.addAttribute("message", ReadFileToString.readFileToArray("description.txt"));
-        return "displayExercise";
+        return "textDescription";
     }
+
+    @GetMapping("/graph")
+    public String indexGraph(Model model) throws IOException {
+        return "graphDescription";
+    }
+
+    @GetMapping("/calc")
+    public String indexCalc(Model model) throws IOException {
+        return "salaryCalculator";
+    }
+
+
+
+
+
 }
 
 
